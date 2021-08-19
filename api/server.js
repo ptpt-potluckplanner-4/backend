9e2e4a51fb6cad59a0ac9747cf0d33adf0ce0f22
HanselV <hanselviva@gmail.com>
2021-08-18 21:52:52 -0700

    setup

---
+++ api/server.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+require("dotenv").config();
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
+const server = express();
+
+const authRouter = require("./auth/auth-router.js");
+
+server.use(morgan("dev"));
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.use("/auth", authRouter);
+
+//root
+server.get("/", (req, res) => {
+	res.send(`<h2> welcome to potluck-planner API homepage! </h2>`);
+});
+
+// catch all
+server.use("*", (req, res) => {
+	res.status(404).json({ message: "oops! that place doesn't exist!" });
+});
+
+// error handler
+server.use((err, req, res, next) => {
+	const message = err?.message || "something went wrong in the server";
+	const status = err?.status || 500;
+
+	res.status(`${status}`).json({ message, stack: err.stack });
+});
+
+module.exports = server;
