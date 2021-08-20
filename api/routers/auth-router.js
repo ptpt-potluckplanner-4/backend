b14b7f95ec89ee605e8c869c2c81168f6e7a7825
HanselV <hanselviva@gmail.com>
2021-08-20 13:57:52 -0700

    fixed error body not defined

--- api/routers/auth-router.js
+++ api/routers/auth-router.js
@@ -3,7 +3,9 @@ const router = require("express").Router();
 const Users = require("../models/users-model");
 
 router.post("/", (req, res, next) => {
-	Users.addUser()
+	const body = req.body;
+
+	Users.addUser(body)
 		.then((newUser) => {
 			res.status(201).json(newUser);
 		})
